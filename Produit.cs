1d22cee71d106aa5ec336d4080c137372c170343
ASMAA AIT OUGUERD <93409472+iasm2024@users.noreply.github.com>
2023-10-14 20:27:29 +0200

    Add files via upload

---
+++ Produit.cs
@@ -0,0 +1,11 @@
+﻿namespace Produits
+{
+    public class Produit
+    {
+        public int id { get; set; }
+        public required string nom { get; set; }
+        public string? type { get; set; }
+        public string? prix { get; set; }
+        public string? image { get; set; }
+    }
+}
